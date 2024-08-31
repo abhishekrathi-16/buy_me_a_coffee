5414b72ae9ec502c653481ac67f71df5a25e8bd9
mytestaccount-1 <abhishekrathi639@gmail.com>
2024-08-31 23:15:18 +0530

    change key name

--- src/actions/uploadActions.ts
+++ src/actions/uploadActions.ts
@@ -9,7 +9,7 @@ export async function uploadToS3(formData: FormData) {
     region: "eu-north-1",
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY as string,
-      secretAccessKey: process.env.AWS_SECRET_KEY as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
     },
   });
 
